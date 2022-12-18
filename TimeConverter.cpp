d9e4d8e3096cd5eb3956240d416e4058e10277f5
Lajob <ustym.hanyk@ukr.net>
2022-12-18 23:04:40 +0200

    Provided more clarity to time conversion by adding second-hour second-minute minute-hour hour-day equivalence constants

--- TimeConverter.cpp
+++ TimeConverter.cpp
@@ -39,14 +39,21 @@ void TimeConverter::printTime() {
 }
 
 void TimeConverter::timeToSeconds() {
-    second+= minute * 60  + hour *3600;
+    const int secondsInAMinute = 60;
+    const int secondsInAnHour = 3600;
+    second+= minute * secondsInAMinute  + hour *secondsInAnHour;
     minute = 0;
     hour = 0;
 }
 void TimeConverter::secondsToTime() {
 //    divide seconds by the number of seconds in one hour and take the modulo of 24 to account time overflow in 24h format
 //    (i.e. 86399 seconds = 23:59:59, but 86401 seconds = 00:00:01)
-    hour = (second/3600) % 24;
-    minute = (second/60) % 60;
-    second = second %60;
+    const int hoursInADay = 24;
+    const int secondsInAnHour = 60;
+    const int secondsInAMinute = 60;
+    const int minutesInAnHour = 60;
+
+    hour = (second/secondsInAnHour) % hoursInADay;
+    minute = (second/secondsInAMinute) % minutesInAnHour;
+    second = second %secondsInAMinute;
 }
